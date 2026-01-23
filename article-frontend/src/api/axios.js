71b86129c1cda00284b6ebb67562f4f32cf8c3c3
arpit-yadav01 <arpityadav170402@gmail.com>
2026-01-23 14:39:10 +0530

    Use environment-based API URL

--- article-frontend/src/api/axios.js
+++ article-frontend/src/api/axios.js
@@ -1,16 +1,16 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//  baseURL: "https://ai-article-manager.onrender.com/api",
-
-// });
-
-// export default api;
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+ baseURL: "https://ai-article-manager.onrender.com/api",
+
 });
 
 export default api;
+
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_BASE_URL,
+// });
+
+// export default api;
